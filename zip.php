5457d4d6a63754355a3394debaa47d87852cd592
Abdul Hannan <abdulhannan.kum@gmail.com>
2017-10-03 08:42:31 +0600

    Update zip.php

--- zip.php
+++ zip.php
@@ -3,7 +3,10 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to ZIP Archive</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Welcome to ZIP Archive</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <style type="text/css">
         * {
             margin: 0;
